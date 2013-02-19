102b6971e3a7ef9eb98ce0232a51e2e00dd8fe07
Daniel <dantdj@live.co.uk>
2013-02-19 14:18:23 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup (name ='PyDecrypt',
+  	version = '1.0',
+		py_modules=['PyDecrypt'],
+		description = 'MD5 Rainbow Table Generator',
+		author = 'Daniel Temple',
+		author_email = 'dantdj@gmail.com',
+		)
