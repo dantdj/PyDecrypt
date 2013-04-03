8da01dbd1860b9d0a8f3e342cca63000083926ed
dantdj <dantdj@live.co.uk>
2013-04-03 17:07:23 +0200

    Changed decryption method to brute force
    Changed the decryption method of the module to a more brute-force attack (As intended) rather than acting as a rainbow table generator as before. Far less resources used than before.

--- pydecrypt.py
+++ pydecrypt.py
@@ -1,58 +1,25 @@
-import itertools
 import hashlib
-import json
-import os
-import csv
+import itertools
+import string
 
-def MD5Decrypt(value):
-	
-	lookupvalue = value[:16] #Half the MD5 hash to match with hashes in dict
+ 
+def brute_force_md5(target_hash, minimum_char=1, log=True):
 
-	letters = 'abcdefghijklmnopqrstuvwxyz' #Define letters to include the alphabet
-
-	if not os.path.isfile("degenerated.txt"):
-		
-		#Generate the various lists, compile them in to one, output confirmation of creation
-		oneChar = map(''.join, itertools.product(letters, repeat=1))
-		twoChar = map(''.join, itertools.product(letters, repeat=2))
-		threeChar = map(''.join, itertools.product(letters, repeat=3))
-		fourChar = map(''.join, itertools.product(letters, repeat=4))
-		fiveChar = map(''.join, itertools.product(letters, repeat=5))
-		
-		possibleValues = oneChar + twoChar + threeChar + fourChar + fiveChar
-		print "List of possible combinations created..."
-		
-		with open("degenerated.txt", 'w') as f:
-			json.dump(possibleValues, f) # Open degenerated.txt and write possibleValues to it
-			
-		print "... and written to file."
-
-	else: 
-		print "Values list found, moving on..."
-		
-	if not os.path.isfile("diction.csv"):
-		
-		with open("degenerated.txt", 'r') as f:
-			possibleValues = json.load(f)
-		
-		fieldnames = ('md5', 'value')
-		with open('diction.csv', 'wb') as myfile:
-		
-			myWriter = csv.DictWriter(myfile, fieldnames = fieldnames)
-			headers = dict((n,n) for n in fieldnames)
-			myWriter.writerow(headers)
-			
-			for value in possibleValues:
-				myWriter.writerow({'md5' : hashlib.md5(value).hexdigest()[:16], 'value' : value})
-			print "Hash list created and written to file."
-
-	else: 
-		print "Dictionary found, moving on..."
-		
-	with open('diction.csv', 'rb') as myfile:
-		for row in csv.reader(myfile):
-			if row[0] == lookupvalue:
-				print "Key = " + row[1]
-				return row[1]
-				break
-				
+    for i in range(minimum_char, 6):
+        product = itertools.product(string.ascii_lowercase + string.digits, repeat=i)
+        char_space = itertools.imap(''.join, product)
+        if log:
+            print
+            print "Beginning strings of length %d..." % i
+ 
+        for combination in char_space:
+            if hashlib.md5(combination).hexdigest() == target_hash:
+                if log:
+                    print "Found hash at %s" % combination
+                return combination
+ 
+        if log:
+            print "...And searched. No result found."
+  
+if __name__ == "__main__":
+    print brute_force_md5(raw_input("md5 to search for: "))
